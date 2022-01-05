48c7b5e541cae90d63804fa091104072e01e9cd6
Adham Atef Hassan <48416945+Adham45@users.noreply.github.com>
2022-01-05 23:39:17 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-// Bootstrap for react
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -13,38 +13,28 @@ import ListGroup from 'react-bootstrap/ListGroup';
 class App extends Component {
 constructor(props) {
 	super(props);
-
-	// Setting up state
 	this.state = {
 	userInput : "",
 	list:[]
 	}
 }
 
-// Set a user input value
+
 updateInput(value){
 	this.setState({
 	userInput: value,
 	});
 }
 
-// Add item if user input in not empty
+
 addItem(){
 	if(this.state.userInput !== '' ){
 	const userInput = {
-
-		// Add a random id which is used to delete
 		id : Math.random(),
-
-		// Add a user value to list
 		value : this.state.userInput
 	};
-
-	// Update list
 	const list = [...this.state.list];
 	list.push(userInput);
-
-	// reset state
 	this.setState({
 		list,
 		userInput:""
@@ -52,14 +42,10 @@ addItem(){
 	}
 }
 
-// Function to delete item from list use id to delete
+
 deleteItem(key){
 	const list = [...this.state.list];
-
-	// Filter values and leave value which we need to delete
 	const updateList = list.filter(item => item.id !== key);
-
-	// Update list in state
 	this.setState({
 	list:updateList,
 	});
@@ -107,7 +93,6 @@ render(){
 <Row>
 	<Col md={{ span: 5, offset: 4 }}>
 		<ListGroup>
-		{/* map over and print items */}
 		{this.state.list.map(item => {return(
 
 			<ListGroup.Item variant="dark" action
